3e45f2eee3c2d65a68045f7c9bb1ec6c87ca4bb6
Glitch (beneficial-smart-bolt) <none>
2021-04-08 15:03:01 +0000

    📙🌺 Updated with Glitch

--- server.js
+++ server.js
@@ -38,6 +38,18 @@ app.get("/Unit3", (request, response) => {
 app.get("/Unit4", (request, response) => {
   response.sendFile(__dirname + "/views/Unit4.html");
 });
+app.get("/Unit5", (request, response) => {
+  response.sendFile(__dirname + "/views/Unit5.html");
+});
+app.get("/pset1", (request, response) => {
+  response.sendFile(__dirname + "/views/pset1.html");
+});
+app.get("/pset2", (request, response) => {
+  response.sendFile(__dirname + "/views/pset2.html");
+});
+app.get("/pset3", (request, response) => {
+  response.sendFile(__dirname + "/views/pset3.html");
+});
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
